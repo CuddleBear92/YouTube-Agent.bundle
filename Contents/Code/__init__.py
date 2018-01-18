0d5cf2a64011e16aeba29d585c101cd90ac6c3dc
Ryan Villarreal <XjCrazy09@users.noreply.github.com>
2018-01-17 23:15:14 -0500

    Update __init__.py

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -7,6 +7,7 @@ def Start():
 	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (iPad; CPU OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B554a Safari/9537.54'
 	HTTP.Headers['Accept-Language'] = 'en-us'
 
+# change this to Agent.TV when finished with the script
 class YouTubeAgent(Agent.Movies):
 
 	name = 'YouTube'
